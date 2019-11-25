a75e909f1b7c8b07fcf258dafb0ab59143d71190
MalikAltawati <Malik.abdullah.abraheem@gmail.com>
2019-11-25 09:45:16 +0100

    Done

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -8,4 +8,27 @@
 
 var firstNonRepeatedCharacter = function(string) {
   // TODO: your solution here
+  string = string.split('')
+ 	var testing = {}
+ 	var j = 1
+ 	var Tester = 100
+	var Least;
+
+ 	for (var i = 0 ; i < string.length; i++){
+
+		 if(testing[string[i]] === undefined){
+		 	testing[string[i]] = j
+		 }else { 
+		 	testing[string[i]] = testing[string[i]]+1
+		 }
+     }
+
+	for (var key in testing){
+		 if (testing[key] < Tester){
+			Tester = testing[key]
+			Least = key
+		}
+	}
+
+return Least;
 };
