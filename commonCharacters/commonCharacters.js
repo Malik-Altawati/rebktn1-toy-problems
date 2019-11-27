ddcb2c3a17f036f9bb6b9a9ddb9fb381ac7c0d2b
MalikAltawati <Malik.abdullah.abraheem@gmail.com>
2019-11-27 09:42:57 +0100

    toy

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -14,4 +14,37 @@
 
 var commonCharacters = function(string1, string2) {
   // TODO: Your code here!
+  string1 = string1.split('')
+  string2 = string2.split('')
+  var result = ''
+  
+  for ( var i = 0 ; i < string1.length; i++){
+  		for(var j = 0; j < string2.length; j++){
+  			if (string1[i] === string2[j]){
+  				result+= string1[i]
+  				string1.splice(i,1)
+  			}
+  			
+  		}
+  }
+return result
+};
+
+//
+
+var commonCharacters = function(string1, string2) {
+  // TODO: Your code here!
+ string1 = string1.split('')
+ string2 = string2.split('')
+ var result = ''
+  
+string1.forEach(function(element1){
+	string2.forEach(function(element2){
+		if (element1 === element2){
+  			result+= element1
+  		}
+	})
+})
+
+  return result
 };
