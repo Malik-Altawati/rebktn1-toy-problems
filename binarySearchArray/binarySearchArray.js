6836b4657a54b9831598b24e95f976c9e37a92d7
MalikAltawati <Malik.abdullah.abraheem@gmail.com>
2019-12-03 09:27:44 +0100

    toy

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -10,6 +10,39 @@
  * console.log(index); // null
  */
 
+// var binarySearch = function (array, target) {
+// 	for(var i = 0 ; i < array.length; i++){
+// 		if(array[i] === target){
+// 			return i;
+// 		}
+// 	}
+// 	return null;
+// };
+
+//
+
+//binarySearch([1, 2, 3, 4, 5], 4);
 var binarySearch = function (array, target) {
+   var min = 0;
+   var max = array.length - 1;
+   var midPoint;
+   //3
+    while (max > min) {
+        midPoint = Math.ceil((max + min)/2);        
+        if (array[midPoint] === target) {
+            return midPoint;
+        }
+        else if(array[midPoint] < target){
+            min = midPoint++;
+        }
+        else {
+            max = midPoint--;
+        }
+    }
+    
+    return null;
 };
+
+
+
 
