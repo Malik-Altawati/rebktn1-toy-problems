f26c77f22c8168faed34c3292cd39983b58c2f93
MalikAltawati <Malik.abdullah.abraheem@gmail.com>
2019-12-05 09:55:38 +0100

    toy

--- characterFrequency/CharacterFrequency.js
+++ characterFrequency/CharacterFrequency.js
@@ -11,3 +11,53 @@ characterFrequency("mississippi")  -->  [ [ "i", 4 ], [ "s", 4 ], [ "p", 2 ], [ 
 
 characterFrequency("") -->  [ ]
 */
+
+// function characterFrequency(string){
+// 	var temp = []
+// 	var counter = 0
+// 	string = string.split("")
+// 	for(var i = 0; i < string.length;i++){
+// 		for(var j = 0; j < string.length;j++){
+// 			if(string[i] === string[j] ){
+// 				counter++
+// 			}
+// 		}
+// 		temp.push([string[i],counter])
+// 		counter = 0
+// 	}
+
+
+
+// 	
+// 	return temp
+// }
+
+//
+
+ function characterFrequency(string){
+ 	var temp = []
+ 	var counter = 0
+ 	string = string.split("")
+ 	for(var i = 0; i < string.length;i++){
+ 		for(var j = 0; j < string.length;j++){
+ 			if(string[i] === string[j] ){
+ 				counter++
+ 			}
+ 		}
+ 		temp.push([string[i],counter])
+ 		counter = 0
+ 	}
+
+ 	var obj = {}
+ 	for(var i = 0; i < temp.length; i++){
+ 		obj[temp[i][0]] = temp[i][1]
+ 	}
+
+ 	var final = []
+ 	for(var key in obj){
+ 		final.push([key,obj[key]])
+ 	}
+
+
+ 	return final.sort();
+}
