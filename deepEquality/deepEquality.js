8e34a3b8c3b85bd2de97978c714acd671b860462
MalikAltawati <Malik.abdullah.abraheem@gmail.com>
2019-12-12 09:41:53 +0100

    toy

--- deepEquality/deepEquality.js
+++ deepEquality/deepEquality.js
@@ -11,5 +11,48 @@
   * don't worry about handling cyclical object structures.
   *
   */
+
+  
 var deepEquals = function(apple, orange) {
-};
+  var arr1 = []
+  var arr2 = []
+  var output1 = ""
+  var output2 = ""
+
+  
+  for (var key in apple){
+    arr1.push(key,apple[key])
+      for(var keys in apple[key]){
+        arr1.push(keys,apple[key][keys])
+      }
+  }
+
+  for (var key in orange){
+    arr2.push(key,orange[key])
+      for(var keys in orange[key]){
+        arr2.push(keys,orange[key][keys])
+      }
+  }
+
+  var result1 = arr1.filter(function(element1){
+    return (typeof(element1) === "number" || typeof(element1) === "string")
+  })
+
+   var result2 = arr2.filter(function(element2){
+    return (typeof(element2) === "number" || typeof(element2) === "string")
+  })
+
+
+  result1.forEach(function(element1){
+    output1+= element1
+  })
+    result2.forEach(function(element2){
+    output2+= element2
+  })
+
+  return output1 === output2
+}
+
+///
+
+
--- mergeTwoBinaryTrees/merge.js
+++ mergeTwoBinaryTrees/merge.js
@@ -39,24 +39,15 @@ Note: The merging process must start from the root nodes of both trees.
 
 
 
-
-	function TreeNode(val) {
-	  this.val = val;
-	  this.left = this.right = null;
-	}
-
-  var t3 = {
-	val: null,
-	left : null,
-    right : null
-	}
-
 	var mergeTrees = function(t1, t2) {
 
-		t3.val = t1.val + t2.val
+		if (!t1) return t2
+		if (!t2) return t1
 
-			t3.left = TreeNode(t1.left+t2.left)
-			t3.right = TreeNode(t1.right+t2.right)
+		t1.val = t1.val + t2.val
 
-		return t3;
+		t1.left = mergeTrees(t1.left+t2.left)
+		t2.right = mergeTrees(t1.right+t2.right)
+
+		return t1;
 	};
