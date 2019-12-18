71755f0eaf0294e97598353d62271995aa328267
MalikAltawati <Malik.abdullah.abraheem@gmail.com>
2019-12-18 09:35:07 +0100

    done

--- treeBFSelect/treeBFSelect.js
+++ treeBFSelect/treeBFSelect.js
@@ -39,6 +39,26 @@ var Tree = function(value) {
 
 Tree.prototype.BFSelect = function(filter) {
   // return an array of values for which the function filter(value, depth) returns true
+    var array = [];
+  if (filter(this.value, 0)) {  
+    array.push(this.value);
+  }
+  // //////
+  var handler = function(node, depth) {
+    if (node.children) {
+      depth++
+      for (var i = 0; i < node.children.length; i++) {
+        if (filter(node.children[i].value, depth)) {
+          array.push(node.children[i].value);
+        }
+        handler(node.children[i], depth);
+      }
+    }
+  }
+  // //
+   handler(this, 0);
+  // //
+  return array;
 };
 
 /**
@@ -94,3 +114,18 @@ Tree.prototype.removeChild = function(child) {
     throw new Error('That node is not an immediate child of this tree');
   }
 };
+
+//
+     var root1 = new Tree(1);
+     var branch2 = root1.addChild(2);
+     var branch3 = root1.addChild(3);
+     var leaf4 = branch2.addChild(4);
+     var leaf5 = branch2.addChild(5);
+     var leaf6 = branch3.addChild(6);
+     var leaf7 = branch3.addChild(7);
+     root1.BFSelect(function (value, depth) {
+       return value % 2;
+     })
+     // root1.BFSelect(function (value, depth) {
+     //   return depth === 1;
+     // })
